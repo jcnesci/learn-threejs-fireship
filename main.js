73a8d78d591baadb53789cead4f1b9f518e2ac8c
jcnesci <jc.nesci@gmail.com>
2022-09-10 21:00:40 +0900

    Changed torus to standard material to see its color.

--- main.js
+++ main.js
@@ -20,9 +20,8 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshStandardMaterial({
   color: 0xff6347,
-  wireframe: true,
 });
 const torus = new THREE.Mesh(geometry, material);
 
